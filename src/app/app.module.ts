181230c10e16054a84c0b943fbc42cdd084d7ac4
kmerck88 <Kevin.Merck88@gmail.com>
2022-11-10 22:11:00 -0500

    Setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuListComponent } from './menu-list/menu-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuListComponent,
+    TaskFormComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
